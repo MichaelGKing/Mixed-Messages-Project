1e06baf139839e29e13368908561f42795547ba6
Michael King <86289390+MichaelGKing@users.noreply.github.com>
2021-11-26 19:10:25 +0800

    messages written

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,8 +1,8 @@
-let messageOne = ["I smell something funny"];
-let messageTwo = ["it is a rose"];
-let messageThree = ["so it must be destroyed"];
+let partOneChoices = ["I smell something funny", "I see something strange", "I sense some kind of danger", "I cannot move", "I am not sure what that is"];
+let partTwoChoices = ["it is a rose", "it is a rock", "it is an ogre", "it is a beauty", "it is my dog"];
+let partThreeChoices = ["so it must be destroyed!", "so I will play with it.", "so I will slay it!", "so I will smell it.", "so I will marry it!"];
     
 const completeMessage = {
-	     
+	
 }
 
