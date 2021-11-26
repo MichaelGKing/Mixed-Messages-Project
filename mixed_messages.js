9975d7b9932a6d5ebe293f3b0cc26e6637a44cc2
Michael King <86289390+MichaelGKing@users.noreply.github.com>
2021-11-26 19:57:39 +0800

    Added message generator and consolelog of message

--- mixed_messages.js
+++ mixed_messages.js
@@ -2,14 +2,16 @@
 let partOneChoices = ["I smell something funny", "I see something strange", "I sense some kind of danger", "I cannot move", "I am not sure what that is"];
 let partTwoChoices = ["it is a rose", "it is a rock", "it is an ogre", "it is a beauty", "it is my dog"];
 let partThreeChoices = ["so it must be destroyed!", "so I will play with it.", "so I will slay it!", "so I will smell it.", "so I will marry it!"];
+const arrayOfArrays = [partOneChoices, partTwoChoices, partThreeChoices];
+let finalMessage = "";
 
-//object which cotains the randomized message    
-const randomizedMessage = {
-	
-}
-
+//randomly generates a number to select the parts for the message
 const randomNumber = num => {
-	return Math.floor(Math.random() * num) + 1;
+	return Math.floor(Math.random() * num);
 }
 
-console.log(randomNumber(5));
+for (i = 0; i < arrayOfArrays.length; i++) {
+	finalMessage += arrayOfArrays[i][randomNumber(5)] + " ";
+};
+
+console.log(finalMessage);
