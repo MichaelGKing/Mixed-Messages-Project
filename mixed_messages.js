f67831e59cd7cc1459ac6e93a0990e6d049c73e3
Michael King <86289390+MichaelGKing@users.noreply.github.com>
2021-11-26 19:42:03 +0800

    Added random number function to choose message

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,8 +1,15 @@
+//Three messageparts which contain the potential choices of the randomizer
 let partOneChoices = ["I smell something funny", "I see something strange", "I sense some kind of danger", "I cannot move", "I am not sure what that is"];
 let partTwoChoices = ["it is a rose", "it is a rock", "it is an ogre", "it is a beauty", "it is my dog"];
 let partThreeChoices = ["so it must be destroyed!", "so I will play with it.", "so I will slay it!", "so I will smell it.", "so I will marry it!"];
-    
-const completeMessage = {
+
+//object which cotains the randomized message    
+const randomizedMessage = {
 	
 }
 
+const randomNumber = num => {
+	return Math.floor(Math.random() * num) + 1;
+}
+
+console.log(randomNumber(5));
